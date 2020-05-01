e258b06eac759ac3a31866ac3e9b26ea6d7e451d
Max Lewis <max.a.lewis98@gmail.com>
2020-05-01 13:48:03 -0700

    buttons eat/recreate burgers

---
+++ public/assets/js/cats.js
@@ -0,0 +1,62 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEaten = $(this).data("lunch");
+
+    var newLunch = {
+      eaten: newEaten
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/cats/" + id, {
+      type: "PUT",
+      data: newLunch
+    }).then(
+      function() {
+        console.log("changed eaten state to", newLunch);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newCat = {
+      name: $("#ca").val().trim(),
+      sleepy: $("[name=sleepy]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/cats", {
+      type: "POST",
+      data: newCat
+    }).then(
+      function() {
+        console.log("created new cat");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    console.log(id)
+
+    // Send the DELETE request.
+    $.ajax("/api/cats/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted cat", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
