b3b0b62c55fc627e60355046be4f1bb369398e5b
Max Lewis <max.a.lewis98@gmail.com>
2020-05-01 12:20:04 -0700

    itit and MYSQL/config

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (name) VALUES ('Plain');
+INSERT INTO burger (name) VALUES ('Double Double');
+INSERT INTO burger (name) VALUES ('Animal Style');
+INSERT INTO burger (name) VALUES ('Veegitarian');
