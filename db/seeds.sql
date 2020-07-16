a8c249bf50b5f8431aed80de9b8ca1a6837f5b29
Max Lewis <max.a.lewis98@gmail.com>
2020-07-15 18:04:30 -0700

    deleted everything and restarting with react as a base

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO burger (name) VALUES ('Plain');
-INSERT INTO burger (name) VALUES ('Double Double');
-INSERT INTO burger (name) VALUES ('Animal Style');
-INSERT INTO burger (name) VALUES ('Veegitarian');
--- server.js
+++ server.js
@@ -1,29 +1,26 @@
-var express = require("express");
+const express = require("express");
+const path = require("path")
 
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-var app = express();
+// backend routing middleware fix
+// var history = require('connect-history-api-fallback');
+// app.use(history())
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// Serve up static assets on heroku
+if (process.env.NODE_ENV === "production") {
+    // app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build'), function (err) {
+        if (err) res.status(500).send(err)
+    })
+})
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/burger_controller");
-
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+// Start the API server
+app.listen(PORT, () => {
+    console.log(`Hosting on http://localhost:${PORT}/`);
 });
