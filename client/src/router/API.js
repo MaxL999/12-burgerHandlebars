8ce5a1a27e8f1351a687091775c7f1a9ae8805a9
Max Lewis <max.a.lewis98@gmail.com>
2020-07-16 09:17:28 -0700

    pulling over the routing from the old project, nonfunctional

---
+++ client/src/router/API.js
@@ -0,0 +1,61 @@
+// // Make sure we wait to attach our handlers until the DOM is fully loaded.
+// $(function () {
+//     $(".change-eaten").on("click", function (event) {
+//         var id = $(this).data("id");
+//         var newEaten = $(this).data("lunch");
+//         console.log("click")
+//         var newLunch = {
+//             eaten: newEaten
+//         };
+
+//         // Send the PUT request.
+//         $.ajax("/api/food/" + id, {
+//             type: "PUT",
+//             data: newLunch
+//         }).then(
+//             function () {
+//                 console.log("changed eaten state to", newLunch);
+//                 // Reload the page to get the updated list
+//                 location.reload();
+//             }
+//         );
+//     });
+
+//     $(".create-form").on("submit", function (event) {
+//         // Make sure to preventDefault on a submit event.
+//         event.preventDefault();
+//         console.log("click")
+//         var newburger = {
+//             name: $("#burger-name").val().trim(),
+//             eaten: 0
+//         };
+
+//         console.log(newburger)
+
+//         // Send the POST request.
+//         $.ajax("/api/food", {
+//             type: "POST",
+//             data: newburger
+//         }).then(
+//             function () {
+//                 // Reload the page to get the updated list
+//                 location.reload();
+//             }
+//         );
+//     });
+
+//     $(".delete-burger").on("click", function (event) {
+//         var id = $(this).data("id");
+//         console.log("click")
+//         // Send the DELETE request.
+//         $.ajax("/api/food/" + id, {
+//             type: "DELETE"
+//         }).then(
+//             function () {
+//                 console.log("deleted burger", id);
+//                 // Reload the page to get the updated list
+//                 location.reload();
+//             }
+//         );
+//     });
+// });
