21c4a2facee4be6248f0415ae23df3d66372d2fa
Max Lewis <max.a.lewis98@gmail.com>
2020-08-14 14:08:59 -0700

    made new SQL tables and spent half an hour searching for the exact syntax for auto increment

--- schema/burger.sql
+++ schema/burger.sql
@@ -1,9 +1,30 @@
+DROP DATABASE IF EXISTS burger_db;
 CREATE DATABASE burger_db;
 USE burger_db;
 
-CREATE TABLE burger {
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
+CREATE TABLE burger (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(35),
     eaten BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
-}
+);
+
+SELECT * FROM burger
+
+
+CREATE TABLE bun (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    Calories INT,
+    Carbs INT,
+    Protien INT,
+    Fats Int,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE meat LIKE bun;
+CREATE TABLE cheese LIKE bun;
+CREATE TABLE condiment LIKE bun;
+CREATE TABLE vegetable LIKE bun;
+
+SELECT * FROM vegetable
