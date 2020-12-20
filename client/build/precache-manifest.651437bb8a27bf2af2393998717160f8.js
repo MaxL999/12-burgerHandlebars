9494fdefd88a57ca59e911a8a0ad277752624226
Max Lewis <max.a.lewis98@gmail.com>
2020-12-19 21:13:20 -0700

    pushing garbage that will be overwritten soon enough

---
+++ client/build/precache-manifest.651437bb8a27bf2af2393998717160f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1ffbd0bac768a279ebd2d191ce70b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2741681796f866686a0",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "62fc7dc0667900addc78",
+    "url": "/static/js/2.0b47d503.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0b47d503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2741681796f866686a0",
+    "url": "/static/js/main.75fdb9e2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
